0bcc02cd8c681fd09dbc8f71f8636dd8c97ef062
Anderson <yrgenius80@gmail.com>
2022-07-22 18:05:23 +0300

    added some style

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -4,27 +4,41 @@ import './App.css';
 class App extends Component {
     constructor(props) {
         super(props);
-        this.rightAnswer = "";
         this.state = {
             rightAnswer: this.rightAnswer,
+            firstNum: 1,
+            secondNum: 1,
         }
         this.getAnswer = this.getAnswer.bind(this);
     }
 
     getAnswer(min, max){
-        return Math.floor(Math.random() * (max - min) + min );
+        return Math.floor((Math.random() * (max - min + 1)) + min );
     }
 
     generate = () => {        
-        this.setState({rightAnswer: this.getAnswer(1, 10)});
+        let rightAnswer = this.getAnswer(1, 10);
+        console.log('answ ' + rightAnswer);
+        let firstNum = this.getAnswer(1, rightAnswer);
+        if(rightAnswer === firstNum) rightAnswer++;
+        console.log('f ' + firstNum);
+        const secondNum = rightAnswer - firstNum;
+        console.log('sec' + secondNum);
+        
+        this.setState({
+            rightAnswer: this.getAnswer(1, 10),
+            firstNum: firstNum, 
+            secondNum: secondNum
+        });
     }
 
     render() {
         
         return (
             <div className = "content" >
-                <p>{this.state.rightAnswer}</p>
-                <button onClick={this.generate}>Generate</button>
+                <p>{this.state.firstNum} + {this.state.secondNum} = </p>
+                <button className='content__btn' onClick={this.generate}>Generate</button>
+
             </div>
             
         );
