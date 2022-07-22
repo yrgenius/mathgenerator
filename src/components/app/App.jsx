c7ef010d04bdd3009edfb176efe1915e502675fa
Anderson <yrgenius80@gmail.com>
2022-07-22 14:10:15 +0300

    start random

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,11 +1,34 @@
+import { Component } from 'react';
 import './App.css';
 
-function App() {
-    return ( 
-        <div className = "content" >
-            <p>{Math.random()}</p>
-        </div>
-    );
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.rightAnswer = "";
+        this.state = {
+            rightAnswer: this.rightAnswer,
+        }
+        this.getAnswer = this.getAnswer.bind(this);
+    }
+
+    getAnswer(min, max){
+        return Math.floor(Math.random() * (max - min) + min );
+    }
+
+    generate = () => {        
+        this.setState({rightAnswer: this.getAnswer(1, 10)});
+    }
+
+    render() {
+        
+        return (
+            <div className = "content" >
+                <p>{this.state.rightAnswer}</p>
+                <button onClick={this.generate}>Generate</button>
+            </div>
+            
+        );
+    }
 }
 
 export default App;
